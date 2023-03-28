18bd7533266b09311b622981aafbed7bbc0d5d83
channavi <101348717+channavi@users.noreply.github.com>
2023-03-29 00:55:03 +0900

    트리구조 만들기

--- week04_Tree/main.cpp
+++ week04_Tree/main.cpp
@@ -1,56 +1,143 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-class Node {
-public:
-    int value;
-    Node* parent;
-    vector<Node*> children;
 
-    Node(int val) {
-        value = val;
-        parent = nullptr;
-    }
+struct Node {
+	Node* left, * right;
+	int value;
 };
 
-class Tree {
+class BinarySearchTree {
 public:
-    Node* root;
+	BinarySearchTree():root(nullptr){};
+	~BinarySearchTree(){};
 
-    Tree() {
-        root = nullptr;
-    }
+	void addNode(int value);
+	bool searchValue(int value);
+	void removeNode(int value);
+	void show();
+private:
+	Node* root;
+	Node* tail;
 
-    void insert(int val, Node* parent) {
-        Node* newNode = new Node(val);
-        newNode->parent = parent;
-        parent->children.push_back(newNode);
-    }
+	void inOrder(Node* current) {
+		if (current != nullptr) {
+			inOrder(current->left);
+			cout << current->value << endl;
+			inOrder(current->right);
+		}
+	}
+
+	Node* searchMaxNode(Node* node) {
+		if (node == NULL)
+			return NULL;
+		while (node->right != NULL) {
+			node = node->right;
+		}
+		return node;
+	}
+	Node* removeSequence(Node* node, int _value);
+
+	void addNode(int value) {
+		Node *node = new Node();
+		Node* tmpRoot = nullptr;
+
+		node->value = value;
+		if (root == nullptr)
+			root = node;
+		else {
+			Node* ptr = root;
+			while (ptr != nullptr) {
+				tmpRoot = ptr;
+				if (node->value < ptr->value) {
+					ptr = ptr->left;
+				}
+				else {
+					ptr = ptr->right;
+				}
+			}
+			if (node->value < tmpRoot->value)
+				tmpRoot->left = node;
+			else
+				tmpRoot->right = node;
+		}
+	}
+
+	Node* removeSequence(Node* node, int _value) {
+		if (node == nullptr) return node;
+		else if (node->value > _value) {
+			node->left = removeSequence(node->left, _value);
+		}
+		else if (node->value < _value) {
+			node->right = removeSequence(node->right, _value);
+		}
+		else {
+			Node* ptr = node;
+			if (node->right == nullptr && node->left == nullptr) {
+				delete node;
+				node = nullptr;
+			}
+			else if (node->right == nullptr) {
+				node = node->left;
+				delete ptr;
+			}
+			else if (node->left == nullptr) {
+				node = node->right;
+				delete ptr;
+			}
+			else {
+				ptr = searchMaxNode(node -> left);
+				node->value = ptr->value;
+				node->left = removeSequence(node->left, ptr->value);
+			}
+		}
+		return node;
+	}
+
+	void removeNode(int value) {
+		Node *ptr = root;
+		removeSequence(ptr, value);
+	}
+
+	bool searchValue(int value) {
+		Node* ptr = root;
+		Node* tmpRoot = nullptr;
+		while (ptr != nullptr) {
+			if (ptr->value == value) {
+				cout << value << "Found" << endl;
+				return true;
+			}
+			else if (ptr->value > value)
+				ptr = ptr->left;
+			else
+				ptr = ptr->right;
+		}
+		cout << value << "not Found" << endl;
+		return false;
+	}
+
+	int main() {
+		BinarySearchTree* BST = new BinarySearchTree();
+		BST->addNode(1);
+		BST->addNode(3);
+		BST->addNode(6);
+		BST->addNode(9);
+		BST->addNode(12);
+		BST->addNode(17);
+		BST->addNode(23);
+		BST->addNode(15);
+		BST->addNode(5);
+
+		BST->show(); cout << endl;
+		BST->searchValue(4);
+		BST->searchValue(17);
+		BST->removeNode(22);
+
+		BST->show();
+
+		cout << endl;
+		return 0;
+	}
 };
-
-int main() {
-    Tree tree;
-
-    Node* n1 = new Node(1);
-    tree.root = n1;
-
-    tree.insert(2, n1);
-    tree.insert(3, n1);
-
-    Node* n4 = new Node(4);
-    tree.insert(5, n4);
-    tree.insert(6, n4);
-    tree.insert(7, n4);
-    tree.insert(4, n1);
-
-    cout << tree.root->children[0]->value << endl; // 2
-    cout << tree.root->children[1]->value << endl; // 3
-    cout << tree.root->children[2]->value << endl; // 4
-    cout << tree.root->children[2]->children[0]->value << endl; // 5
-    cout << tree.root->children[2]->children[1]->value << endl; // 6
-    cout << tree.root->children[2]->children[2]->value << endl; // 7
-
-    return 0;
-}
